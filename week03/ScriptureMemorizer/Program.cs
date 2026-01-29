42b9b81f037facf946d5982dba066ad217f42391
olaxemax <jibaro4u2004@gmail.com>
2026-01-29 22:21:38 +0100

    week 03

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,144 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // Example scripture: John 3:16
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference,
+            "For God so loved the world that he gave his one and only Son, " +
+            "that whoever believes in him shall not perish but have eternal life.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress ENTER to hide words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nAll words are hidden. Program ending...");
+                break;
+            }
+        }
+    }
+}
+
+// -------------------- Reference Class --------------------
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int? _endVerse; // optional for ranges
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse.HasValue)
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+    }
+}
+
+// -------------------- Word Class --------------------
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+}
+
+// -------------------- Scripture Class --------------------
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(" ").Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords()
+    {
+        // Hide 3 random words each time
+        for (int i = 0; i < 3; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} - {scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
     }
 }
